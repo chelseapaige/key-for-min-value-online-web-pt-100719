a62b14d5d3a66471492f1c59abb49206745a5de4
Chelsea Hansen <chelseahansen@gmail.com>
2019-10-22 06:24:35 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,11 +1,12 @@
 passengers = {
-   suite_a: "Hazel",
-   suite_b: "Ryan",
-   suite_c: "Ben",
-
+   suite_a: "Amanda Presley",
+   suite_b: "Seymour Hoffman",
+   suite_c: "Alfred Tennyson",
+   suite_d: "Charlie Chaplin",
+   suite_e: "Crumpet the Elf"
 }
 
-def select_winner(family)
+def select_winner(passengers)
    winner = "" #given empty value to get the right value
    passengers.each { |suite, name|
       if suite == :suite_a && name.start_with?("A")
